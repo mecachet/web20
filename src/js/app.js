4e76f742f4850ead81b5f38d20023527730bdf68
mecachet <otoqvilitaia@gmail.com>
2023-08-11 23:51:57 +0400

    changed code

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,53 @@
-console.log("Js linked. Yay");
-const createUserUrl = "https://borjomi.loremipsum.ge/api/register",
-  getAllUsersUrl = "https://borjomi.loremipsum.ge/api/all-users",
-  getSingleUserUrl = "https://borjomi.loremipsum.ge/api/get-user/1 ",
-  updateUserUrl = "https://borjomi.loremipsum.ge/api/update-user/1 ",
-  deleteUserUrl = "https://borjomi.loremipsum.ge/api/delete-user/1";
+console.log("linked yay");
 
-const userName = document.querySelector("#user-name"),
-  userSurname = document.querySelector("#user-surname"),
-  userEmail = document.querySelector("#user-email"),
-  userPhone = document.querySelector("#user-mobile"),
-  userPersonalID = document.querySelector("#user-personal"),
-  userZip = document.querySelector("#user-zip"),
-  userGender = document.querySelector("#user-gender"),
-  const user_id = document.querySelector("#user-id").value;
+function modalAction(selector) {
+  const modal = document.querySelector(selector);
+  const closeBtn = modal.querySelector(".modal-close");
+  modal.classList.add("open");
+  closeBtn.addEventListener("click", () => {
+    modal.classList.remove("open");
+  });
+}
 
-function userActions() {}
+const openRegFormBtn = document.querySelector("#open-reg-form");
+
+openRegFormBtn.addEventListener("click", () => {
+  modalAction("#reg-modal");
+});
+
+const createUserUrl = "https://borjomi.loremipsum.ge/api/register";
+const getAllUsersUrl = "https://borjomi.loremipsum.ge/api/all-users";
+const getSingleUserUrl = "https://borjomi.loremipsum.ge/api/get-user/1";
+const updateUserUrl = "https://borjomi.loremipsum.ge/api/update-user/1";
+const deleteUserUrl = "https://borjomi.loremipsum.ge/api/delete-user/1";
+
+const regForm = document.querySelector("#reg"),
+  userName = document.querySelector("#user_name"),
+  userSurname = document.querySelector("#user_surname"),
+  userEmail = document.querySelector("#user_email"),
+  userPhone = document.querySelector("#user_phone"),
+  userPersonalID = document.querySelector("#user_personal-id"),
+  userZip = document.querySelector("#user_zip-code"),
+  userGender = document.querySelector("#user_gender"),
+  user_id = document.querySelector("#user_id");
+
+const user = {
+  first_name: "steso",
+  last_name: "text",
+  phone: "123456789",
+  id_number: "12345678909",
+  email: "text@gmail.com",
+  gender: "male",
+  zip_code: "1245",
+};
+
+const editBtn = document.querySelector("#editBtn");
+const deleteBtn = document.querySelector("#deleteBtn");
+
+function userActions() {
+  editBtn.addEventListener("click", updateUser);
+  deleteBtn.addEventListener("click", deleteUser);
+}
 
 function renderUsers(usersArray) {
   console.log(usersArray);
@@ -25,13 +58,31 @@ function getAllUsers() {
   fetch("https://borjomi.loremipsum.ge/api/all-users")
     .then((res) => res.json())
     .then((data) => {
+      console.log(data);
       const users = data.users;
-      console.log(users);
       renderUsers(users);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally(() => {
+      console.log("finally");
     });
 }
 
-getAllUsers();
+function deleteUser(id) {
+  fetch(`http://borjomi.loremipsum.ge/api/delete-user/${id}`, {
+    method: "delete",
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      getAllUsers();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
 function getUserInfo(id) {
   fetch(`https://borjomi.loremipsum.ge/api/get-user/${id}`)
@@ -40,92 +91,82 @@ function getUserInfo(id) {
       console.log(data);
     });
 }
-function userActions() {
-  const editButtons = document.querySelectorAll(".edit-btn");
-  const deleteButtons = document.querySelectorAll(".delete-btn");
-  editButtons.forEach((button) => {
-    button.addEventListener("click", async (event) => {
-      const selectedElement = event.currentTarget;
-      const userId = selectedElement.dataset.userId;
 
-      const userData = await getUserInfo(userId);
+function updateUser(userObj) {
+  fetch(`http://borjomi.loremipsum.ge/api/update-user/${userObj.id}`, {
+    method: "PUT",
+    body: JSON.stringify(userObj),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
 
-      fillFormWithUserData(userData);
-
-      showModal();
-
-      const updateButton = document.getElementById("update-button");
-      updateButton.addEventListener("click", () => {
-        const updatedUserData = getUpdatedUserDataFromForm();
-        updateUser(userId, updatedUserData);
-
-        closeModal();
-      });
+function createUser(userObj) {
+  fetch("https://borjomi.loremipsum.ge/api/register", {
+    method: "POST",
+    body: JSON.stringify(userObj),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      getAllUsers();
     });
-  });
-
-  deleteButtons.forEach((button) => {
-    button.addEventListener("click", async (event) => {
-      const selectedElement = event.currentTarget;
-      const userId = selectedElement.dataset.userId;
-
-      await deleteUser(userId);
-
-      updateUIAfterDelete(userId);
-    });
-  });
 }
 
+getAllUsers();
 
-async function getUserInfo(userId) {
-}
+regForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-function fillFormWithUserData(userData) {
-}
+  const userObj = {
+    id: user_id.value,
+    first_name: userName.value,
+    last_name: userSurname.value,
+    phone: userPhone.value,
+    id_number: userPersonalID.value,
+    email: userEmail.value,
+    gender: userGender.value,
+    zip_code: userZip.value,
+  };
 
-function showModal() {
-}
+  createUser(userObj);
+});
 
-function getUpdatedUserDataFromForm() {
-}
-
-async function updateUser(userId, updatedUserData) {
-}
-
-async function deleteUser(userId) {
-}
-
-function updateUIAfterDelete(userId) {
-}
-async function updateUser(userObj) {
+async function getAllUsersAsync() {
   try {
-    const response = await fetch(`https://borjomi.loremipsum.ge/api/update-user/${userObj.id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(userObj)
-    });
-
-    if (response.ok) {
-      await fetchAndRenderUsers();
-    } else {
-      console.error("Failed to update user:", response.status, response.statusText);
-    }
+    const response = await fetch(
+      "https://borjomi.loremipsum.ge/api/all-users",
+      {
+        method: "get",
+      }
+    );
+    const data = await response.json();
+    console.log(data);
   } catch (error) {
-    console.error("An error occurred:", error);
+    console.log(error);
+  } finally {
+    console.log("finally");
   }
 }
-async function fetchAndRenderUsers() {
-  try {
-    const response = await fetch("https://borjomi.loremipsum.ge/api/all-users");
-    if (response.ok) {
-      const usersArray = await response.json();
-      renderUsers(usersArray);
-    } else {
-      console.error("Failed to fetch users:", response.status, response.statusText);
-    }
-  } catch (error) {
-    console.error("An error occurred:", error);
-  }
+
+function addPost() {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
 }
